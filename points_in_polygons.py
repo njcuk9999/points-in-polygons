db83e5ce93427942332b71a96def12be5bd5813d
Neil Cook <neil.james.cook@gmail.com>
2016-06-01 09:55:08 +0100

    Update points_in_polygons.py

--- points_in_polygons.py
+++ points_in_polygons.py
@@ -7,7 +7,7 @@ import matplotlib.path as mplPath
 # =============================================================================
 # Define functions
 # =============================================================================
-def mask_from_polygons(xarr, yarr, polygons, polygons_in):
+def mask_from_polygons(xarr, yarr, polygons, polygons_in, include_holes=True):
   """
   Takes a list of polygons and returns a array of bools (mask) where [i]
   is True if xarr[i] and yarr[i] are inside all polygons.
@@ -51,58 +51,67 @@ def mask_from_polygons(xarr, yarr, polygons, polygons_in):
                       where:
 
                       polygon_in = [True, False, ..., True]
+                      
+    :param include_holes: bool, whether we count polygons that lie inside
+                          (i.e. have polygons_in[i][j] = True) as holes
+                          and thus points inside these polygons are not
+                          counted as inside the polygons
 
-
-  :return insideany: array of bools for, mask True where xarr[i] and yarr[i] are inside the polygon
+  :return ms: list of arrays (length of polygons), each array is an array of 
+              bools for, a mask  True where xarr[i] and yarr[i] are inside the polygon
   """
   xyarr = np.array(zip(xarr, yarr))
   falsearray = np.array([False] * len(xarr), dtype=bool)
   insideany = falsearray.copy()
+  ms = []
   for k in range(len(polygons)):
+      # get this polygon set
       polygon = polygons[k]
-      # +++++++++++++++++++++++++++++++++++++++++++++
       # deal with annoying contained polygons
       if polygons_in is None:
           polygon_in = None
       else:
           polygon_in = polygons_in[k]
-      # +++++++++++++++++++++++++++++++++++++++++++++
+      # copy inside any from previously created false array
+      insideany = falsearray.copy()
+      # loop round each set of vertices in this polygon
       for j in range(len(polygon)):
-          poly = polygon[j]
-          # mask out the points outside the poly clip box
-          pmax_x, pmax_y = np.max(poly, axis=0)
-          pmin_x, pmin_y = np.min(poly, axis=0)
+          # get the vertices for this polygon
+          vertices = polygon[j]
+          # mask out the points outside the poly clip box (this is quicker
+          # than running the full set of points through Path.contains_points)
+          pmax_x, pmax_y = np.max(vertices, axis=0)
+          pmin_x, pmin_y = np.min(vertices, axis=0)
           mask1 = (xarr > pmin_x) & (xarr < pmax_x)
           mask2 = (yarr > pmin_y) & (yarr < pmax_y)
           mask = mask1 & mask2
           # if no points inside poly clip box then don't bother counting
           if len(mask[mask]) == 0:
               continue
-          # -----------------------------------------------------------------
-          # deal with annoying contained polygons
+          # deal with polygons contained within polygons
+          # (must be known before runtime)
           if polygon_in is None:
               poly_in = False
           else:
               poly_in = polygon_in[j]
-          # -----------------------------------------------------------------
           # Creates a mask for points (xs, ys) based on whether they are
           # inside a polygon poly from http://stackoverflow.com/a/23453678
-          bbPath = mplPath.Path(poly)
+          bbPath = mplPath.Path(vertices)
+          # inside = point_inside_polygon_multi3(xarr, yarr, poly)
           inside = falsearray.copy()
           inside[mask] = bbPath.contains_points(xyarr[mask])
-          # -----------------------------------------------------------------
           # if polygon is inside another polygon (as defined by poly_in)
           # do not count it as inside
           if poly_in and not include_holes:
               insideany &= ~inside
           else:
               insideany |= inside
-          # -----------------------------------------------------------------
-  return insideany
+      ms.append(insideany)
+  return ms
+
 
 # count the number of points inside a list of "polygon collections"
-def count_objects_inside(polygons, xarr, yarr, polygons_in=None,
-                         include_holes=True):
+def count_objects_inside(polygons, xarr, yarr, polygons_in=None, include_holes=True):
     """
     Takes a list of polygons and counts how many points are inside.
     Points are defined by xarr, yarr such that we look whether
